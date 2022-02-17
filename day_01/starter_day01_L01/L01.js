63de63b48fee423ec404ccb71b8bee0b27d6cd77
BEGUE <beguethomas439@gmail.com>
2022-02-17 15:46:52 +0400

    Exercice L01

--- day_01/starter_day01_L01/L01.js
+++ day_01/starter_day01_L01/L01.js
@@ -1,3 +1,26 @@
 var defi = 'Pratiquer les algorithmes en JavaScript';
-
 console.log(defi)
+
+
+
+var names = 'Pratiquer les algorithmes en JavaScript'
+var namelength = names.length
+console.log(namelength)
+
+
+console.log(names.toUpperCase())
+
+console.log(names.toLowerCase())
+
+console.log(names.substring(9))
+
+
+var man = "jours en JavaScript"
+var mine = 10;
+var string = 
+
+console.log(mine + man)
+console.log(string.includes(searchvalue, start))
+
+let word = "Jours en JavaScript from 10 jours en JavaScript."
+console.log(word.includes("Script"));
