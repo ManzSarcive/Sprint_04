0772e38bd386fa82f0835f3cc0e3c4028a125ea2
BEGUE <beguethomas439@gmail.com>
2022-02-18 09:45:31 +0400

    Exercice L01 fini

--- day_01/starter_day01_L01/L01.js
+++ day_01/starter_day01_L01/L01.js
@@ -17,10 +17,26 @@ console.log(names.substring(9))
 
 var man = "jours en JavaScript"
 var mine = 10;
-var string = 
 
 console.log(mine + man)
-console.log(string.includes(searchvalue, start))
+// console.log(string.includes(searchvalue, start))
 
 let word = "Jours en JavaScript from 10 jours en JavaScript."
 console.log(word.includes("Script"));
+
+console.log(word.split("", 3));
+console.log(word.replace("JavaScript", "PHP"));
+console.log(word.charAt(15));
+console.log(word.charCodeAt(1));
+console.log(word.indexOf("JavaScript"));
+console.log(word.lastIndexOf("JavaScript"))
+
+var phrase = "Vous ne pouvez pas terminer une phrase avec parceque parceque c'est une conjonction"
+
+console.log(phrase.indexOf("parceque"))
+console.log(phrase.lastIndexOf("parceque"))
+console.log(phrase.search("parceque"))
+
+console.log(word.trim(""))
+
+
