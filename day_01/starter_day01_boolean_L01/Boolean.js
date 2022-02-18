ee11af5d6ed553acfcb2056085d4b2e9bfc1d10b
BEGUE <beguethomas439@gmail.com>
2022-02-18 14:01:01 +0400

    exercice boolean

---
+++ day_01/starter_day01_boolean_L01/Boolean.js
@@ -0,0 +1,44 @@
+// var firstName = "jean"; 
+// var lastName = "bob"
+// var country = "France"
+// var age = 16
+// var isMarried = "true"
+// var year = "2016"
+
+// console.log(typeof(firstName))
+// console.log(typeof(lastName))
+// console.log(typeof(age))
+// console.log(typeof(country))
+// console.log(typeof(isMarried))
+// console.log(typeof(year))
+// console.log(typeof("10"))
+// console.log(typeof(parseFloat('9.8')))
+
+// var num = 0;
+// var obj = new String("0");
+// var str = "0";
+
+// console.log(num === num); 
+// console.log(obj === obj); 
+// console.log(str === str); 
+
+// console.log(num === obj); 
+// console.log(num === str); 
+// console.log(obj === str); 
+// console.log(null === undefined); 
+// console.log(obj === null);  
+// console.log(obj === undefined); 
+console.log(4 > 3) //true
+console.log(4 >= 3) //true
+console.log(4 < 3) //false
+console.log(4 <= 3) //false
+console.log(4 == 4) //true
+console.log(4 === 4) //true
+console.log(4 != 4) //false
+console.log(4 !== 4) //false
+console.log(4 != '4') //false
+console.log(4 == '4') //true
+console.log(4 === '4') // false
+
+
+
