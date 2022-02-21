1e42faabe1c0000408ff0338e24bc3b4074818f0
Thomas <beguethomas439@gmail.com>
2022-02-21 13:16:51 +0400

    Exercice 02 fini

--- day_02/exercice -2.js
+++ day_02/exercice -2.js
@@ -14,8 +14,8 @@
 // console.log(mixedDataTypes.length)
 
 
-let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
-// console.log(itCompanies)
+// let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+// // console.log(itCompanies)
 // console.log(itCompanies[0]); 
 // console.log(itCompanies[4]); 
 // console.log(itCompanies[itCompanies.length - 1]); 
@@ -33,3 +33,39 @@ let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 
 // console.log(itCompanies.toString)
 // itCompanies.sort
 // console.log(itCompanies.sort)
+
+// for (i=0;i<=6;i++);
+// console.log(i)
+// for (let number = 0; number < 5; number++) {
+//     console.log(number);
+//   }
+
+
+// let i = 0
+// while (i < 5) {
+//     i++;
+//     console.log(i)
+//   }
+// function table_multiplication(nombre)
+// 		{
+// 		var i;
+// 		for (i=0; i<=10; i++) {
+// 		    resultat=nombre*i;
+// 			                  }
+// 		}
+//         console.log()
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+// console.log(person)
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+//   let chien = {name:'foxy', pattes: '4', color: 'white', age: '2', proprierty: 'little'};
+//   console.log(chien)
