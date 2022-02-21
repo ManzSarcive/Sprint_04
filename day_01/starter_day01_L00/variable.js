846d0584d323bd9f5248b22c11fa79896567ebc9
Thomas <beguethomas439@gmail.com>
2022-02-21 11:07:33 +0400

    Exercice Day02

--- day_01/starter_day01_L00/variable.js
+++ day_01/starter_day01_L00/variable.js
@@ -1,12 +1,25 @@
-// *****Exercices: Data Types --> Level00 [4]*****
-
-
-// Declaring different variables of different data types
-
+var boolean = true;
+var undifined = undifined;
+var rouge = null;
+var text = "bonjour";
 
 
 
 // Declaring variables with number values
+var b = "hello";
+var bonjour = "bonjour";
+var noob = "noob";
+var trois = 3;
+// Declaring variables without number values
+var b, bonjour, noob, trois;
+
+
+
+var prenom = "thomas";
+var nom = "BEGUE";
+var matrimonial ="celibataire";
+var pays = "France";
+var age = 23;
 
 
 
@@ -14,5 +27,14 @@
 
 
 // Variables can also be declaring in one line separated by comma
+var prenom = "thomas", nom = "BEGUE", matrimonial = "celibataire", pays = "France", age = 23;
 
+    console.log(prenom, nom, matrimonial, pays, age);
+
+
+
+var defi = "Pratiquerles algorythmes en JavaScript";
+// console.log(defi);
+// console.log(length)
+console.log("alphabet".toUpperCase());
 
---
+++ day_02/exercice -2.js
@@ -0,0 +1,35 @@
+// let fruits = ['Apple', 'Banana', 'apple', 'strawberry', 'mango'];
+
+// console.log(fruits.length);
+
+// fruits.push('Mango');
+
+
+// let pos = fruits.indexOf('Banana');
+
+// console.log(pos)
+
+// let mixedDataTypes = ['cake', 'apple', 'table', 'chair', 'bike', 'makeup'];
+
+// console.log(mixedDataTypes.length)
+
+
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies)
+// console.log(itCompanies[0]); 
+// console.log(itCompanies[4]); 
+// console.log(itCompanies[itCompanies.length - 1]); 
+
+// itCompanies.forEach(function(item, index, array) {
+//     console.log(item, index);
+//   });
+
+// console.log(itCompanies)
+
+// console.log('Facebook'.toUpperCase())
+// console.log('Google'.toUpperCase())
+// console.log('Microsoft'.toUpperCase())
+// console.log('Apple'.toUpperCase())
+// console.log(itCompanies.toString)
+// itCompanies.sort
+// console.log(itCompanies.sort)
